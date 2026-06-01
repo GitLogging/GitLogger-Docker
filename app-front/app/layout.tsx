d2eed66961e227c3d343b72dc4ebe416fa20dfef
Jake Bolton <ninmonkeys@gmail.com>
2026-06-01 15:44:33 -0500

    clean: remove template html

--- app-front/app/layout.tsx
+++ app-front/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next"
 import { Geist, Geist_Mono } from "next/font/google"
 import "./globals.css"
+// import "./gitlogger-base.css"
+// import "./gitlogger-4bitcss-iceberg-dark.css"
 
 const geistSans = Geist({
     variable: "--font-geist-sans",
