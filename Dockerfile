1bd2360013d1522e79178ba5261a480f45e74c9f
Jake Bolton <ninmonkeys@gmail.com>
2026-06-29 16:12:39 -0500

    add: `Dockerfile` copy pwsh template files

---
+++ Dockerfile
@@ -0,0 +1,72 @@
+# FROM mcr.microsoft.com/powershell:7.4-alpine-3.18
+FROM mcr.microsoft.com/powershell AS powershell
+
+# runs powershell server under /app
+# RUN apk add --no-cache gh
+
+# install packages, and clean up temp files
+#
+#   ex: cache version syntax:
+#       apt-get install -y git=1:* gh=* && \
+RUN --mount=type=cache,target=/var/cache/apt \
+    --mount=type=cache,target=/var/lib/apt/lists \
+    apt-get update && \
+    apt-get install -y git && \
+    apt-get install -y gh && \
+    apt-get clean && \
+    rm -rf /var/lib/apt/lists/*
+
+# Set the module name to the name of the module we are building
+# ENV ModuleName=WebSocket
+# ENV InstallAptGet="git","curl","ca-certificates","libc6","libgcc1"
+ENV InstallModule="ugit,pansies"
+# Copy the module into the container
+RUN --mount=type=bind,src=./,target=/app-gitserve /bin/pwsh -nologo -command /docker-build/Initialize/Container.init.ps1
+# Set the entrypoint to the script we just created.
+
+
+# Notes:
+#
+# Powershell modules are installed to:
+#   /root/.local/share/powershell/Modules
+
+
+# ENV PWSH_HOST=127.0.0.1
+# ENV PWSH_HOST=0.0.0.0
+ENV PWSH_HOST=*
+ENV PWSH_PORT=3001
+
+ENV APPDIR=/app
+WORKDIR $APPDIR
+
+COPY /app /app
+# SHELL ["pwsh", "-NoProfile", "-NoLogo", "-Command"]
+
+RUN --mount=type=bind,src=.,target=/app
+EXPOSE 3001
+# if process before node, then:
+    # CMD ["pwsh", "-NoProfile", "-NoLogo", "-File", "/app/server-routing.ps1"]
+    # CMD ["pwsh", "-NoProfile", "-NoLogo", "-NoExit", "-File", "/app/min-server.ps1"]
+
+    # RUN pwsh -Nop -Nol -F .\app\server-run.ps1 -Port 3002 -Host 127.0.0.1
+    # RUN ./app/server-run.ps1 -Port 3002 -Host 127.0.0.1
+    # CMD ["pwsh", "-NoProfile", "-NoLogo", "-File", "server-run.ps1"]
+    # RUN pwsh -F ./server-run.ps1 -Port 3000 -Host '127.0.0.1'
+
+# or serve as default command
+# Note: You might want to use '-NoExit'
+#   if you are using "docker run -it tag"
+#   but not when using "docker run tag"
+# ENTRYPOINT [ "/bin/pwsh","-file","/app/server-run.ps1" ]
+ENTRYPOINT [ "/bin/pwsh","-noexit","-file","/app/server-run.ps1" ]
+# ENTRYPOINT [ "/bin/pwsh","-nologo","-noexit","-file","/Container.start.ps1" ]
+
+# RUN --mount=type=bind,src=/app,target=/app2
+# RUN --mount=type=bind,target=. go build -o /myapp ./cmd
+# RUN npm install --omit=dev
+# CMD ["node", "src/index.js"]
+# SHELL ["pwsh", "-NoLogo", "-Command"]
+# CMD [ "Get-Location | Write-Host" ]
+# CMD ["pwsh", "-NoProfile", "-NoLogo", "-File", "/app/server-run.ps1"]
+# CMD ["pwsh", "-Command", "'hi world'|write-host"]
+# CMD ["pwsh", "-NoProfile", "-NoLogo", "-File", "server-run.ps1"]
---
+++ docker-build/RebuildRun-Container.ps1
@@ -0,0 +1,82 @@
+<#
+.SYNOPSIS
+    stop all containers, rebuild, open url, then run
+.EXAMPLE
+    # defaults
+    .\docker-rebuild-run.ps1
+
+    # open Url plus shell mode
+    .\docker-rebuild-run.ps1 -AutoOpenUrl http://127.0.0.1:3001/repos/list -InteractiveMode
+
+    # skip entering shell
+    .\docker-rebuild-run.ps1 -AutoOpenUrl http://127.0.0.1:3001/repos/list
+
+.EXAMPLE
+    manually using interactive mode:
+    docker run --interactive --tty --publish 127.0.0.1:3001:3001 $ImageName
+.notes
+# 1-liner for REPL to stop, rebuild, open browser, and run it:
+
+if( docker ps -q ) { docker stop $( docker ps -q ) } ; docker build -t $ImageName . && (start-process -FilePath 'http://127.0.0.1:3001') && docker run -it --publish 127.0.0.1:3001:3001 $ImageName # stop all containers, rebuild, open url, then run
+#>
+param(
+    [ArgumentCompletions(
+        'http://127.0.0.1:3001/repo/list',
+        'http://127.0.0.1:3001',
+        'http://127.0.0.1:3001/repo/clone?url=https://github.com/BurntSushi/ripgrep.git'
+    )]
+    [string[]] $AutoOpenUrl = 'http://127.0.0.1:3001/repo/list',
+
+    [string] $ImageName = 'gitlogger-docker',
+
+    # runs: docker -it
+    [switch] $InteractiveMode
+)
+$script:ImageName = $ImageName
+
+"Stopping all running containers..." | Write-Host -fg 'green'
+if( docker ps -q ) {
+    docker stop $( docker ps -q )
+}
+
+# only launch if build has no errors.
+# --tty allows caller to ctrl+c to kill it.
+# note: the extra '@()' is intentional, it allows the '&& operators' to chain a nested expression
+
+
+( $MyId =
+    docker run `
+        --interactive `
+        --publish 127.0.0.1:3000:3000 `
+        -w /app --mount "type=bind,src=.,target=/app" `
+        'gitlogger-docker'
+)
+
+# '-v', 'H:\RootClonedRepos'
+
+docker build -t $ImageName . &&
+    (
+        @( foreach( $url in $AutoOpenUrl ) {
+            "Opening URL: ${url}" | Write-Host -fg 'magenta'
+            start-process -FilePath $Url
+        })
+    ) && (
+        @( if( $InteractiveMode ) {
+            docker run --interactive --tty `
+                --publish 127.0.0.1:3001:3001 `
+                -v cloned-repos:h:\RootClonedRepos `
+                -w /app --mount "type=bind,src=.,target=/app" `
+                $ImageName
+            # docker run --interactive --tty --publish 127.0.0.1:3001:3001 $ImageName
+            # $entryPoint = "/bin/pwsh","-noexit", "-file","/app/server-run.ps1"
+            # docker run -interactive --tty --publish 127.0.0.1:3001:3001 $ImageName @entryPoint
+        } else {
+            docker run --tty --rm `
+                --publish 127.0.0.1:3001:3001
+                -w /app --mount "type=bind,src=.,target=/app" `
+                -v cloned-repos:h:/RootClonedRepos `
+                $ImageName
+        })
+    )
+
+"`aBuild Complete! ImageName: ${ImageName}" | Write-Host -fg 'green'
