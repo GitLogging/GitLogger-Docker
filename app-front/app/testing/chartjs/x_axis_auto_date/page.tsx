7076948a52a54b810313e1f8c015ba8955cdbfec
Jake Bolton <ninmonkeys@gmail.com>
2026-07-27 13:56:00 -0500

    test: example comparing with and without date-fn when rendering

--- app-front/app/testing/chartjs/x_axis_auto_date/page.tsx
+++ app-front/app/testing/chartjs/x_axis_auto_date/page.tsx
@@ -458,7 +458,7 @@ function Chart5() {
                 /> */}
                 <CustomBarMetric
                     DatasetConfig={config1}
-                    ChartTitle="4:1:A | axisPerMonth"
+                    ChartTitle="5:1:A | axisPerMonth"
                     ChartConfig={axisPerMonth}
                     Options={{
                         AutoSplitDatasets: false,
@@ -472,13 +472,122 @@ function Chart5() {
 
 }
 
+function Chart6() {
+    // "http://127.0.0.1:3001/repo/metric/commit?name=junegunn/fzf&since=12.months&period=month",
+    //             "http://127.0.0.1:3001/repo/metric/commit?name=BurntSushi/ripgrep&since=12.months&period=month",
+    const configTotal1 = [
+        {
+            // RequestUrl: `http://127.0.0.1:3001/repo/metric/commit?name=ninmonkey/GitServed&since=12.months&period=day`,
+            RequestUrl: `http://127.0.0.1:3001/repo/metric/totalcommit?name=BurntSushi/ripgrep&period=day&since=5.months`,
+            XAxisKey: `XAxisKey`,
+            YAxisKey: `TotalCommits`,
+            DatasetLabel: `Total Commits`
+        },
+    ]
+
+    // const opts1: ChartOptions<'bar'> = {
+    //     scales: {
+    //         x: {
+    //             stacked: false,
+    //             // stacked: true,
+    //             type: 'category',
+    //         },
+    //         y: {
+    //             stacked: true,
+    //         },
+    //     },
+    // }
+    // const axisPerDay: ChartOptions<'bar'> = {
+    //     scales: {
+    //         x: {
+    //             type: 'time',
+    //             time: {
+    //                 parser: "yyyy-MM-dd'T'HH:mm:ssxxx",  // ISO 8601 with timezone offset (date-fns format)
+    //                 unit: 'day',
+    //                 displayFormats: {
+    //                     day: 'yyyy-MM-dd'
+    //                 },
+    //                 ticks: {
+    //                     maxTicksLimit: 20
+    //                 }
+    //             }
+    //         }
+    //     }
+    // }
+    // const axisPerMonth: ChartOptions<'bar'> = {
+    //     scales: {
+    //         x: {
+    //             type: 'time',
+    //             time: {
+    //                 parser: "yyyy-MM-dd'T'HH:mm:ssxxx",  // ISO 8601 with timezone offset (date-fns format)
+    //                 unit: 'month',
+    //                 displayFormats: {
+    //                     month: 'MMM yyyy'
+    //                 },
+    //                 // ticks: {
+    //                 //     maxTicksLimit: 12
+    //                 // }
+    //             }
+    //         }
+    //     }
+    // }
+    const axisPerDay: ChartOptions<'bar'> = {
+        scales: {
+            x: {
+                type: 'time',
+                time: {
+                    // parser: "yyyy-MM-dd'T'HH:mm:ssxxx",  // ISO 8601 with timezone offset (date-fns format)
+                    parser: "yyyy-MM-dd",  // ISO 8601 with timezone offset (date-fns format)
+                    unit: 'day',
+                    displayFormats: {
+                        day: 'yyyy-MM-dd',
+                        // year: 'yyyy',
+                    },
+                },
+            },
+
+        },
+
+
+    }
+
+    return (
+
+        <>
+            <article>
+
+                <h3>auto date axis</h3>
+                <CustomBarMetric
+                    DatasetConfig={configTotal1}
+                    ChartTitle="6:1:A | totalCommits"
+                />
+                <h3>date-fn date axis</h3>
+                <CustomBarMetric
+                    DatasetConfig={configTotal1}
+                    ChartTitle="6:1:B | totalCommits"
+                    ChartConfig={axisPerDay}
+                // ChartConfig={axisPerMonth}
+                // Options={{
+                //     AutoSplitDatasets: false,
+                // }}
+                // ChartConfig={opts1}
+                />
+            </article>
+        </>
+
+    )
+
+}
+
+
 export default function Page() {
     return (
         <>
             <PageHeaderContent />
-            <Chart5 />
-            <Chart4 />
-            <Chart3 />
+            <Chart6 />
+            {/* <Chart5 /> */}
+            {/* <Chart4 />
+            <Chart3 /> */}
             {/* <Chart1 /> */}
             {/*
              */}
