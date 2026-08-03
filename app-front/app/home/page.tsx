8d62cdb1246753286fa5ee6894e79b8a1fb04f06
Jake Bolton <ninmonkeys@gmail.com>
2026-08-02 20:55:54 -0500

    clean: format code

--- app-front/app/home/page.tsx
+++ app-front/app/home/page.tsx
@@ -1,12 +1,12 @@
-"use client";
+"use client"
 // import { useState, Suspense, useEffect } from "react"
-import type { ChartOptions } from "chart.js";
-import { useMemo, useState } from "react";
-import { ButtonGroup, Dropdown, DropdownButton } from "react-bootstrap";
-import { BarMetric } from "@/app/components/charts/BarMetric";
-import { CustomBarMetric } from "@/app/components/charts/CustomBarMetric";
+import type { ChartOptions } from "chart.js"
+import { useMemo, useState } from "react"
+import { ButtonGroup, Dropdown, DropdownButton } from "react-bootstrap"
+import { BarMetric } from "@/app/components/charts/BarMetric"
+import { CustomBarMetric } from "@/app/components/charts/CustomBarMetric"
 import { CustomLineMetric } from "@/app/components/charts/CustomLineMetric"
-import { PageHeaderContent } from "@/app/components/PageHeaderContent";
+import { PageHeaderContent } from "@/app/components/PageHeaderContent"
 import { DefaultDeserializer } from "v8"
 import { Line } from "react-chartjs-2"
 
@@ -18,170 +18,170 @@ import { Line } from "react-chartjs-2"
 // import { FlatLineChart } from "@/app/components/charts/FlatLineChart"
 
 function PageSummary() {
-  return (
-    <>
-      <article>
-        <BarMetric
-          RequestUrl={[
-            "http://127.0.0.1:3001/repo/metric/commit?name=ninmonkey/GitServed&period=day",
-            // "http://127.0.0.1:3001/repo/metric/commit?name=ninmonkey/Mintils.ps1&since=12.months&period=day",
-            // "http://127.0.0.1:3001/repo/metric/commit?name=startAutomating/GitLogger&since=12.months&period=day",
-            // "http://127.0.0.1:3001/repo/metric/commit?name=StartAutomating/GitLogger&since=30.months&period=day",
-            // "http://127.0.0.1:3001/repo/metric/commit?name=StartAutomating/pssvg&since=30.months&period=month",
-          ]}
-        />
+    return (
+        <>
+            <article>
+                <BarMetric
+                    RequestUrl={[
+                        "http://127.0.0.1:3001/repo/metric/commit?name=ninmonkey/GitServed&period=day",
+                        // "http://127.0.0.1:3001/repo/metric/commit?name=ninmonkey/Mintils.ps1&since=12.months&period=day",
+                        // "http://127.0.0.1:3001/repo/metric/commit?name=startAutomating/GitLogger&since=12.months&period=day",
+                        // "http://127.0.0.1:3001/repo/metric/commit?name=StartAutomating/GitLogger&since=30.months&period=day",
+                        // "http://127.0.0.1:3001/repo/metric/commit?name=StartAutomating/pssvg&since=30.months&period=month",
+                    ]}
+                />
 
-        <BarMetric
-          RequestUrl={[
-            "http://127.0.0.1:3001/repo/metric/commit?name=ninmonkey/GitServed&since=12.months&period=month",
-            "http://127.0.0.1:3001/repo/metric/commit?name=ninmonkey/Mintils.ps1&since=12.months&period=month",
-            // "http://127.0.0.1:3001/repo/metric/commit?name=startAutomating/GitLogger&since=12.months&period=month",
-            // "http://127.0.0.1:3001/repo/metric/commit?name=StartAutomating/emoji&since=30.months&period=month",
-            // "http://127.0.0.1:3001/repo/metric/commit?name=StartAutomating/pssvg&since=30.months&period=month",
-          ]}
-        />
-        <BarMetric
-          RequestUrl={[
-            "http://127.0.0.1:3001/repo/metric/commit?name=ninmonkey/GitServed&since=30.months&period=day",
-            // "http://127.0.0.1:3001/repo/metric/commit?name=ninmonkey/Mintils.ps1&since=12.months&period=day",
-            // "http://127.0.0.1:3001/repo/metric/commit?name=startAutomating/GitLogger&since=12.months&period=day",
-            "http://127.0.0.1:3001/repo/metric/commit?name=StartAutomating/GitLogger&since=30.months&period=day",
-            // "http://127.0.0.1:3001/repo/metric/commit?name=StartAutomating/pssvg&since=30.months&period=month",
-          ]}
-        />
-        <BarMetric
-          RequestUrl={[
-            "http://127.0.0.1:3001/repo/metric/commit?name=StartAutomating/GitLogger&since=30.months&period=month",
-            "http://127.0.0.1:3001/repo/metric/commit?name=StartAutomating/pssvg&since=30.months&period=month",
-          ]}
-        />
-        <BarMetric
-          RequestUrl={[
-            "http://127.0.0.1:3001/repo/metric/commit?name=StartAutomating/GitLogger&since=30.months&period=day",
-            // "http://127.0.0.1:3001/repo/metric/commit?name=StartAutomating/pssvg&since=30.months&period=month",
-          ]}
-        />
-      </article>
-    </>
-  );
+                <BarMetric
+                    RequestUrl={[
+                        "http://127.0.0.1:3001/repo/metric/commit?name=ninmonkey/GitServed&since=12.months&period=month",
+                        "http://127.0.0.1:3001/repo/metric/commit?name=ninmonkey/Mintils.ps1&since=12.months&period=month",
+                        // "http://127.0.0.1:3001/repo/metric/commit?name=startAutomating/GitLogger&since=12.months&period=month",
+                        // "http://127.0.0.1:3001/repo/metric/commit?name=StartAutomating/emoji&since=30.months&period=month",
+                        // "http://127.0.0.1:3001/repo/metric/commit?name=StartAutomating/pssvg&since=30.months&period=month",
+                    ]}
+                />
+                <BarMetric
+                    RequestUrl={[
+                        "http://127.0.0.1:3001/repo/metric/commit?name=ninmonkey/GitServed&since=30.months&period=day",
+                        // "http://127.0.0.1:3001/repo/metric/commit?name=ninmonkey/Mintils.ps1&since=12.months&period=day",
+                        // "http://127.0.0.1:3001/repo/metric/commit?name=startAutomating/GitLogger&since=12.months&period=day",
+                        "http://127.0.0.1:3001/repo/metric/commit?name=StartAutomating/GitLogger&since=30.months&period=day",
+                        // "http://127.0.0.1:3001/repo/metric/commit?name=StartAutomating/pssvg&since=30.months&period=month",
+                    ]}
+                />
+                <BarMetric
+                    RequestUrl={[
+                        "http://127.0.0.1:3001/repo/metric/commit?name=StartAutomating/GitLogger&since=30.months&period=month",
+                        "http://127.0.0.1:3001/repo/metric/commit?name=StartAutomating/pssvg&since=30.months&period=month",
+                    ]}
+                />
+                <BarMetric
+                    RequestUrl={[
+                        "http://127.0.0.1:3001/repo/metric/commit?name=StartAutomating/GitLogger&since=30.months&period=day",
+                        // "http://127.0.0.1:3001/repo/metric/commit?name=StartAutomating/pssvg&since=30.months&period=month",
+                    ]}
+                />
+            </article>
+        </>
+    )
 }
 
 interface DateFnOption {
-  unit: "day" | "week" | "month" | "year";
+    unit: "day" | "week" | "month" | "year"
 }
 function ShowTop5() {
-  /**
-   * $list_repos | sort NewestCommitDate -Descending | select -First 5
-   */
+    /**
+     * $list_repos | sort NewestCommitDate -Descending | select -First 5
+     */
 
-  const yourTop5 = [
-    "ninmonkey/GitServed",
-    "junegunn/fzf",
-    "microsoft/vscode",
-    "powershell/powershelleditorservices",
-    "burntsushi/ripgrep",
-  ];
+    const yourTop5 = [
+        "ninmonkey/GitServed",
+        "junegunn/fzf",
+        "microsoft/vscode",
+        "powershell/powershelleditorservices",
+        "burntsushi/ripgrep",
+    ]
 
-  const period = `day`;
-  const since = `2.months`;
-  // const dateFnUnit_Slicer =
-  const [dateFnOption, setDateFnOption] =
-    useState<DateFnOption["unit"]>(`week`);
+    const period = `month`
+    const since = `2.months`
+    // const dateFnUnit_Slicer =
+    const [dateFnOption, setDateFnOption] =
+        useState<DateFnOption["unit"]>(`week`)
 
-  const configTotal1 = useMemo(
-    () =>
-      yourTop5.map((repo) => ({
-        RequestUrl: `http://127.0.0.1:3001/repo/metric/totalcommit?name=${repo}&period=${period}&since=${since}`,
-        XAxisKey: `XAxisKey`,
-        YAxisKey: `TotalCommits`,
-        DatasetLabel: repo.split("/")[1],
-      })),
-    [],
-  );
+    const configTotal1 = useMemo(
+        () =>
+            yourTop5.map((repo) => ({
+                RequestUrl: `http://127.0.0.1:3001/repo/metric/totalcommit?name=${repo}&period=${period}&since=${since}`,
+                XAxisKey: `XAxisKey`,
+                YAxisKey: `TotalCommits`,
+                DatasetLabel: repo.split("/")[1],
+            })),
+        [],
+    )
 
-  const axisPerWeek: ChartOptions<"bar"> = useMemo(
-    () => ({
-      scales: {
-        x: {
-          stacked: true,
-          type: "time",
-          time: {
-            parser: "yyyy-MM-dd",
-            unit: dateFnOption,
-            displayFormats: {
-              day: "yyyy-MM-dd",
+    const axisPerWeek: ChartOptions<"bar"> = useMemo(
+        () => ({
+            scales: {
+                x: {
+                    stacked: true,
+                    type: "time",
+                    time: {
+                        parser: "yyyy-MM-dd",
+                        unit: dateFnOption,
+                        displayFormats: {
+                            day: "yyyy-MM-dd",
+                        },
+                    },
+                },
             },
-          },
-        },
-      },
-    }),
-    [dateFnOption],
-  );
+        }),
+        [dateFnOption],
+    )
 
-  return (
-    <>
-      <article>
-        <h3>Your Top 5</h3>
-        <CustomBarMetric
-          DatasetConfig={configTotal1}
-          ChartTitle="Your Top 5 | totalCommits"
-        />
-        <h3>
-          Your top 5 (using <strong>date-fn</strong> per <strong>week</strong>)
-        </h3>
-        <CustomBarMetric
-          DatasetConfig={configTotal1}
-          ChartTitle="Your Top 5 | totalCommits (date-fn per week)"
-          ChartConfig={axisPerWeek}
-          // ChartConfig={axisPerMonth}
-          // Options={{
-          //     AutoSplitDatasets: false,
-          // }}
-          // ChartConfig={opts1}
-        />
-        <ButtonGroup>
-          <DropdownButton
-            as={ButtonGroup}
-            title={"date-fn: " + dateFnOption}
-            id="bg-nested-dropdown"
-          >
-            <Dropdown.Item
-              onClick={(e) => {
-                e.preventDefault();
-                setDateFnOption("day");
-              }}
-            >
-              Day
-            </Dropdown.Item>
-            <Dropdown.Item
-              onClick={(e) => {
-                e.preventDefault();
-                setDateFnOption("week");
-              }}
-            >
-              Week
-            </Dropdown.Item>
-            <Dropdown.Item
-              onClick={(e) => {
-                e.preventDefault();
-                setDateFnOption("month");
-              }}
-            >
-              Month
-            </Dropdown.Item>
-            <Dropdown.Item
-              onClick={(e) => {
-                e.preventDefault();
-                setDateFnOption("year");
-              }}
-            >
-              Year
-            </Dropdown.Item>
-          </DropdownButton>
-        </ButtonGroup>
-      </article>
-    </>
-  );
+    return (
+        <>
+            <article>
+                <h3>Your Top 5</h3>
+                <CustomBarMetric
+                    DatasetConfig={configTotal1}
+                    ChartTitle="Your Top 5 | totalCommits"
+                />
+                <h3>
+                    Your top 5 (using <strong>date-fn</strong> per <strong>week</strong>)
+                </h3>
+                <CustomBarMetric
+                    DatasetConfig={configTotal1}
+                    ChartTitle="Your Top 5 | totalCommits (date-fn per week)"
+                    ChartConfig={axisPerWeek}
+                // ChartConfig={axisPerMonth}
+                // Options={{
+                //     AutoSplitDatasets: false,
+                // }}
+                // ChartConfig={opts1}
+                />
+                <ButtonGroup>
+                    <DropdownButton
+                        as={ButtonGroup}
+                        title={"date-fn: " + dateFnOption}
+                        id="bg-nested-dropdown"
+                    >
+                        <Dropdown.Item
+                            onClick={(e) => {
+                                e.preventDefault()
+                                setDateFnOption("day")
+                            }}
+                        >
+                            Day
+                        </Dropdown.Item>
+                        <Dropdown.Item
+                            onClick={(e) => {
+                                e.preventDefault()
+                                setDateFnOption("week")
+                            }}
+                        >
+                            Week
+                        </Dropdown.Item>
+                        <Dropdown.Item
+                            onClick={(e) => {
+                                e.preventDefault()
+                                setDateFnOption("month")
+                            }}
+                        >
+                            Month
+                        </Dropdown.Item>
+                        <Dropdown.Item
+                            onClick={(e) => {
+                                e.preventDefault()
+                                setDateFnOption("year")
+                            }}
+                        >
+                            Year
+                        </Dropdown.Item>
+                    </DropdownButton>
+                </ButtonGroup>
+            </article>
+        </>
+    )
 }
 function ShowTopLine() {
     /**
@@ -190,10 +190,10 @@ function ShowTopLine() {
 
     const yourTop5 = [
         "ninmonkey/GitServed",
+        "GitLogging/GitLogger-Docker",
         // "junegunn/fzf",
         // "microsoft/vscode",
         // "powershell/powershelleditorservices",
-        "burntsushi/ripgrep",
     ]
 
     const period = `day`
@@ -234,10 +234,10 @@ function ShowTopLine() {
     return (
         <>
             <article>
-                <h3>Your Commits</h3>
+                <h3>Recent for Ninmonkey</h3>
                 <CustomLineMetric
                     DatasetConfig={configTotal1}
-                    ChartTitle="Your Top 5 | totalCommits"
+                    ChartTitle="recent: Nin | totalCommits"
                 // ChartConfig={axisPerWeek}
                 />
                 <ButtonGroup>
@@ -286,12 +286,13 @@ function ShowTopLine() {
 }
 
 export default function Page() {
-  return (
-    <>
-      <PageHeaderContent />
-          <ShowTopLine />
-      <ShowTop5 />
-      {false && <PageSummary />}
-    </>
-  );
+    return (
+        <>
+            <PageHeaderContent />
+            <ShowTopLine />
+            {true && <ShowTop5 />}
+            {false && <PageSummary />}
+        </>
+    )
+
 }
